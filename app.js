15c3dd01419d8d4032ad5cc4630843832af97c1d
Jesús Merino <jmmerino@gmail.com>
2015-08-08 12:10:28 +0200

    Fix error in api routes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
         cookieParser = require("cookie-parser"),
         bodyParser = require("body-parser"),
         routes = require("./routes/index"),
-        users = require("./routes/api"),
+        api = require("./routes/api"),
         app = express();
 
     // view engine setup
@@ -24,7 +24,7 @@
     app.use(express.static(path.join(__dirname, "public")));
 
     app.use("/", routes);
-    app.use("/users", users);
+    app.use("/api/v0.1", api);
 
     // catch 404 and forward to error handler
     app.use(function(req, res, next) {
--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,7 @@
         request = require("request"),
         busal = require("../busal");
 
-    router.get("/api/v0.1/lines", function(req, res) {
+    router.get("/lines", function(req, res) {
         var url = "http://salamanca.twa.es/code/getlineas.php";
 
         request(url, function(error, response, html) {
