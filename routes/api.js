b430bbf82d991fc2625eca1fdfd8d54ee622b2fa
Jesús Merino <jmmerino@gmail.com>
2015-08-08 16:40:16 +0200

    Added api stops request

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,9 @@
     var express = require("express"),
         router = express.Router(),
         request = require("request"),
+        jschardet = require("jschardet"),
+        Iconv  = require("iconv").Iconv,
+        charset = require("charset"),
         busal = require("../busal");
 
     router.get("/lines", function(req, res) {
@@ -14,6 +17,89 @@
                 res.json(busal.parseBusLine(html));
             }
         });
+    });
+
+    router.get("/stops", function(req, res) {
+        var busStopsDir1 = [],
+            busStopsDir2 = [],
+            opt;
+
+        opt = {
+            url: "http://salamanca.twa.es/code/getparadas.php?idl=" + req.query.idl,
+            headers: { Referer: "http://salamanca.twa.es/" }
+        };
+
+        request(opt, function(error, response, html) {
+
+            var busStops = busal.parseBusStops(html),
+                contParsedBusStops = 0;
+
+            busStops.forEach(function(busStop) {
+
+                busStop.idl = req.query.idl;
+
+                var opt = {
+                    url: "http://salamanca.twa.es/code/getparadas.php?idl=" + busStop.idl + "&idp=" + busStop.idp + "&ido=" + busStop.ido,
+                    encoding: "binary",
+                    headers: {
+                        Referer: "http://salamanca.twa.es/"
+                    }
+                };
+
+                request(opt, function(error, response, html) {
+
+                    var enc = charset(res.headers, html),
+                        iconv;
+
+                    enc = enc || jschardet.detect(html).encoding.toLowerCase();
+
+                    if (enc != "utf-8"){
+                        iconv = new Iconv(enc, "UTF-8//TRANSLIT//IGNORE");
+                        html = iconv.convert(new Buffer(html, "binary")).toString("utf-8");
+                    }
+
+                    contParsedBusStops++;
+                    busStop = busal.parseBusStop(html, busStop);
+
+                    if (parseInt(busStop.direction) === 0){
+                        busStopsDir2.push(busStop);
+                    } else {
+                        busStopsDir1.push(busStop);
+                    }
+
+                    if (contParsedBusStops === busStops.length){
+
+                        busStopsDir1.sort(function(a, b) {
+                            if ( parseInt(a.id) > parseInt(b.id)){
+                                return 1;
+                            }
+                            if ( parseInt(a.id) < parseInt(b.id)){
+                                return -1;
+                            }
+                            return 0;
+                        }).reverse();
+
+                        busStopsDir2.sort(function(a, b) {
+                            if ( parseInt(a.id) > parseInt(b.id)){
+                                return 1;
+                            }
+                            if ( parseInt(a.id) < parseInt(b.id)){
+                                return -1;
+                            }
+                            return 0;
+                        });
+
+                        res.json({
+                            direction1: busStopsDir1,
+                            direction2: busStopsDir2
+                        });
+                    }
+                });
+
+            });
+
+        });
+
     });
 
     module.exports = router;
