1458d34276f26d9ed71e6ba1692dfada97611bb4
Jesús Merino <jmmerino@gmail.com>
2016-01-10 16:07:58 +0100

    Added new routes to navigate trough lines and stops

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,28 @@ var fdwp;
     var express = require("express"),
         router = express.Router();
 
-    router.get("/home", function(req, res) {
+    router.get("/", function(req, res) {
         res.render("index");
     });
+    router.get("/lines", function(req, res) {
+        res.render("index");
+    });
+    router.get("/lines/:lineRef", function(req, res) {
+        var lineRef = req.params.lineRef;
+
+        res.render("index", {
+            lineRef: lineRef
+        });
+    });
+
+    router.get("/stop/:stopRef", function(req, res) {
+        var stopRef = req.params.stopRef;
+
+        res.render("index", {
+            stopRef: stopRef
+        });
+    });
+
     router.get("/fav", function(req, res) {
         res.render("index");
     });
